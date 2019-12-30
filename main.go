7c4eb4a6289d20152d2c512aac897835cc9e8852
Ernesto Colombo <ernesto.colombo@gmail.com>
2019-12-30 15:52:19 +0100

    first image

--- main.go
+++ main.go
@@ -7,11 +7,24 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+var millenniumImg *ebiten.Image
+
+func init() {
+	var err error
+	millenniumImg, _, err = ebitenutil.NewImageFromFile("res/millennium.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("could not load millennium image: %v", err)
+	}
+}
+
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// ebitenutil.DebugPrint(screen, "Hello, World!")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(365, 252)
+	screen.DrawImage(millenniumImg, op)
 	return nil
 }
 
